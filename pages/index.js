7746efd248d7bef1ca217982fabd1a4ff9b351ac
faridaalin <farida.alin@yahoo.com>
2021-06-03 08:08:45 +0200

    WIP: filtering

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  SimpleGrid,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItemOption,
+  MenuOptionGroup,
+  Button,
+  HStack,
+  Box,
+} from '@chakra-ui/react';
 import client from '../lib/apollo-client';
 import { GET_ALL_FOODTRUCKS } from '../graphql/queries';
 import Card from '../components/card';
@@ -7,8 +18,61 @@ import Layout from '../components/layout';
 const Home = ({ foodtrucks }) => {
   const currentFoodtrucks = foodtrucks ? foodtrucks : [];
 
+  const requests = [...currentFoodtrucks];
+  function flatten(arr) {
+    return arr.flat(Infinity);
+  }
+  const categories = requests.map((element) => {
+    return element.categories.map((item) => item.name);
+  });
+  const flattenedCategories = flatten(categories);
+
+  const uniqueCategories = flattenedCategories.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  });
+
   return (
     <Layout title='Home'>
+      <Box>
+        <HStack spacing='24px'>
+          <Menu closeOnSelect={false}>
+            <MenuButton as={Button} colorScheme='blue'>
+              Filter
+            </MenuButton>
+            <MenuList minWidth='240px'>
+              <MenuOptionGroup
+                title='Category'
+                type='checkbox'
+                onChange={(e) => console.log('Filter value:', e)}
+              >
+                {uniqueCategories.map((category) => (
+                  <MenuItemOption value={category}>{category}</MenuItemOption>
+                ))}
+
+                <MenuItemOption value='country'>Country</MenuItemOption>
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+
+          <Menu closeOnSelect={false}>
+            <MenuButton as={Button} colorScheme='blue'>
+              Sort
+            </MenuButton>
+            <MenuList minWidth='240px'>
+              <MenuOptionGroup
+                defaultValue='asc'
+                title='Rating'
+                type='radio'
+                onChange={(e) => console.log('Sort value:', e)}
+              >
+                <MenuItemOption value='asc'>Ascending</MenuItemOption>
+                <MenuItemOption value='desc'>Descending</MenuItemOption>
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+        </HStack>
+      </Box>
+
       {currentFoodtrucks.length === 0 ? (
         <Text>
           Sorry, we don't have any foodtrucks available now. Try again later.
